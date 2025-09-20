4db5fd47058589bc7f6757cd723a381564f63f87
nataly <19nat.9295@gmail.com>
2025-09-20 01:35:19 -0500

    subir cambios antiguos

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,5 @@
-
-
 import React, { useEffect, useState } from "react";
-import { getTareas } from "../services/taskService";
+import { getTareas } from "../utils/service";
 
 const TaskList = () => {
   const [tareas, setTareas] = useState([]);
